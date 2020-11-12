152441d8f8a01820f60568d0dbf3ba9ac6772cf9
Nur Ahmad Khatim <61086300+naimackerman@users.noreply.github.com>
2020-11-12 18:50:37 +0800

    add scroll function

--- script.js
+++ script.js
@@ -9,3 +9,23 @@ function visitorFunction() {
     }
     document.getElementById("yourname").innerHTML = txt;
 }
+
+myButton = document.getElementById("myBtn");
+
+window.onscroll = function() {
+    scrollFunction();
+}
+
+function scrollFunction() {
+    if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
+        myButton.style.display = "block";
+    }
+    else {
+        myButton.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
